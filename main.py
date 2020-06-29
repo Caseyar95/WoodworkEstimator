768670f6c7609d6a0a53178eec9356da98820c33
Caseyar95 <casey.robertson95@gmail.com>
2020-06-28 23:42:44 -0500

    Update main.py
    Basic functionality testing

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ class Client:
     def __init__(self,name):
         self.name = name
 
-    def getName(self):
-        return self.name
+    # def getName(self):
+    #     return self.name
 
 class Wood:
     def __init__(self,woodType,bf):
@@ -14,7 +14,7 @@ class Wood:
         self.bf = bf
         self.bfCost = 0
 
-    def findBfCost(self, location):
+    def setBfCost(self, location):
         wb = xlrd.open_workbook(location)
         sheet = wb.sheet_by_index(0)
         for rows in range(sheet.nrows):
@@ -27,31 +27,59 @@ class Wood:
     def getBfCost(self):
         return self.bfCost
 
-    def getWoodType(self):
-        return self.wood
+    # def getWoodType(self):
+    #     return self.wood
 
-    def getBf(self):
-        return self.bf
+    # def getBf(self):
+    #     return self.bf
 
     def costForWood(self, waste):
-        return (self.bf * self.bfCost * waste)
+        return (self.bf * self.bfCost + (waste * self.bf))
 
 
 class Project:
-    def __init__(self,hours,projId):
+    def __init__(self,projId,hours):
         self.hours = hours
         self.projId = projId
         self.cost = 0
+        self.hourlyRate = 13
+        self.miscRate = .1
     
-    def getProj(self):
-        return self.projId
+    # def getProj(self):
+    #     return self.projId
 
-    def getHours(self):
-        return self.hours
+    # def getHours(self):
+    #     return self.hours
+
+    def setRate(self, rate):
+        self.hourlyRate = rate
+
+    def setMiscRate(self, misc):
+        self.miscRate = misc
     
-    def getCost(self, woodCost, miscCost, laborCost):
-        self.cost = woodCost + miscCost + (laborCost * self.hours)
+    def getCost(self, woodCost):
+        self.cost = (woodCost * 3) + (woodCost * self.miscRate) + (self.hourlyRate * self.hours)
         return self.cost
 
+client1 = Client("Casey Robertson")
 
+wood1 = Wood("Walnut", 10)
+wood1.setBfCost("woodspecies.xlsx")
+woodC = wood1.costForWood(.2)
+
+sideTable = Project("side table", 10)
+sideTable.setRate(10)
+sideTable.setMiscRate(.05)
+projectCost = sideTable.getCost(woodC)
+
+
+#test
+print(client1.name)
+print(wood1.wood, wood1.bf, round(wood1.getBfCost(), 2))
+print(sideTable.projId, sideTable.hours, round(projectCost, 2))
+
+workTup = (wood1.wood, wood1.bf, sideTable.projId, sideTable.hours, sideTable.cost)
+workQueue = {client1.name : workTup}
+
+print(workQueue[client1.name])
 
